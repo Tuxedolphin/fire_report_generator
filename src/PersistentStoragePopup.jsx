f3c87c909b944019026cb78e6e1f7132d282b056
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-09-29 22:00:41 +0800

    Added Request For Persistent Storage and Alert

---
+++ src/PersistentStoragePopup.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+import { Alert, Collapse, IconButton } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+
+const StoragePopup = () => {
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    let ignore = false;
+    
+    if (!ignore) {
+      if (navigator.storage && navigator.storage.persist) {
+        navigator.storage.persist().then((persistent) => {
+          if (persistent) {
+            console.log("Persistent Storage Granted");
+          } else {
+            setShowPopup(true);
+          }
+        });
+      }
+    }
+
+    return () => { ignore = true };
+  },[]);
+
+  return (
+    <Collapse in={showPopup}>
+        <Alert
+          severity="warning"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setShowPopup(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Persistent storage was not granted to the website. Hence, there is a chance that the data saved will be wiped
+          by the browser should the website not be accessed after a prolonged period of time.
+        </Alert>
+      </Collapse>
+  )
+}
+
+export default StoragePopup
