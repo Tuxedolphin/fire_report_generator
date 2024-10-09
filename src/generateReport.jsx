1b66b0db0a666d445f716f424bab28e26f8f99ca
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-09 20:44:12 +0800

    Added Basic Template For Drawing Annexes

--- src/generateReport.jsx
+++ src/generateReport.jsx
@@ -3,7 +3,7 @@ import pptxgen from "pptxgenjs";
 /**
  * Functions increment the alphabet by one and returns the result
  * 
- * @param {Char} c 
+ * @param {char} c 
  * @returns The next character in the alphabet
  */
 function nextChar(c) {
@@ -55,7 +55,15 @@ async function generateReport() {
       { x: 0.6653543, y: 0.4291339, h: 0.6968504, w: 5.5472441, fontSize: 12 }
     );
 
-    slide.addText(`ANNEX ${annex}`, { x: 6.2165354331, y: 0.4291339, fontSize: 12 });
+    slide.addText(`ANNEX ${annex}`, {
+      x: 6.2165354331,
+      y: 0.4291339,
+      h: 0.3425197,
+      w: 1.14173,
+      fontSize: 15,
+      bold: true,
+      underline: true
+    });
 
     slide.addText(
       [
@@ -89,20 +97,74 @@ async function generateReport() {
       line: { width: 2.25 }
     });
 
-    slide.addShape(pptx.shapes.LINE, {
-      x: 4.1653543,
-      y: 9.2637795,
-      w: 0,
-      h: 1,
-      line: { width: 2.25 }
+    // For the location of "sketch" and "legend" text
+    let sketchX, legendX, y;
+
+    if (layout) {
+      slide.addShape(pptx.shapes.LINE, {
+        x: 3.334646,
+        y: 9.2637795,
+        w: 0,
+        h: 1,
+        line: { width: 2.25 }
+      });
+
+      y = 9.2637795;
+      sketchX = 0.4173228;
+      legendX = 3.354331;
+
+    } else {
+      slide.addShape(pptx.shapes.LINE, {
+        x: 4.1653543,
+        y: 9.2637795,
+        w: 0,
+        h: 1,
+        line: { width: 2.25 }
+      });
+
+      y = 9.3385827;
+      sketchX = 0.5826772;
+      legendX = 4.3188976;
+    }
+
+    slide.addText("NOT DRAWN TO SCALE", {
+      x: 0.6338583,
+      y: 8.9685039,
+      w: 1.480315,
+      h: 0.23622,
+      fontSize: 8,
+      color: "919191",
+      underline: true,
     });
+
+    slide.addText("SKETCH:", {
+      x: sketchX,
+      y: y,
+      h: 0.2598425,
+      w: 0.826772,
+      fontSize: 10,
+      underline: true
+    });
+    
+    slide.addText("LEGEND:", {
+      x: legendX,
+      y: y,
+      h: 0.2598425,
+      w: 0.826772,
+      fontSize: 10,
+      underline: true
+    });
+
+    annex = nextChar(annex);
 
   }
 
   // Adds the front annexes
   if (c1Acc) {
     let slide = pptx.addSlide();
-    formatDrawingTemplate(slide);
+    formatDrawingTemplate(slide, false);
+    let newSlide = pptx.addSlide();
+    formatDrawingTemplate(newSlide, true);
   } else {
 
   }
