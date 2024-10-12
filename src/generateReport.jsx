72b32a51fd13cc39f04ba41ed8e02b93141765c0
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-12 23:00:32 +0800

    Refactored Code And Added Basics Of Photo Annex Generation

--- src/generateReport.jsx
+++ src/generateReport.jsx
@@ -1,5 +1,39 @@
 import pptxgen from "pptxgenjs";
 
+//Defining constants for global use - note that let is used to update it every time the function is called
+let bag, c1Acc, incNumb, location, postalCode;
+
+let pptx;
+
+let annex = "A";
+let pageNumb = 1;
+
+/**
+ * Retrieves and updates the variables to be that from localStorage
+ */
+function updateBasicConstants() {
+  //Getting constants from local storage
+  bag = localStorage.getItem("bagNumb");
+  c1Acc = (localStorage.getItem("c1acc") === null) ? false : JSON.parse(localStorage.getItem("c1acc"));
+  incNumb = localStorage.getItem("incidentNumb");
+  location = localStorage.getItem("location").toUpperCase();
+  postalCode = (localStorage.getItem("postalCode"));
+}
+
+/**
+ * Creates a new pptxgen object and formats its basic values
+ */
+function formatPptx() {
+  
+  pptx = new pptxgen();
+  
+  pptx.subject = "Fire Report";
+  pptx.theme = { headFontFace: "Arial", bodyFontFace: "Arial" };
+  pptx.defineLayout({ name:'A4', width:7.5, height:11.0244094 });
+  pptx.layout = "A4";
+  
+}
+
 /**
  * Functions increment the alphabet by one and returns the result
  * 
@@ -9,7 +43,383 @@ import pptxgen from "pptxgenjs";
 function nextChar(c) {
   return String.fromCharCode(c.charCodeAt(0) + 1);
 }
-nextChar('a');
+
+/**
+ * 
+ * @param {Image} image The image object for calculating the aspect ratio
+ * @returns The aspect ratio
+ */
+function getAspectRatio(image) {
+
+  const w = image.naturalWidth;
+  const h = image.naturalHeight;
+
+  let aspectRatio;
+
+  if (w > h) {
+      aspectRatio = w / h;
+  } else {
+      aspectRatio = h / w;
+  }
+
+  return aspectRatio;
+
+};
+
+/**
+   * Adds the basic formatting to each slide. Specific values taken with reference from original ppt (in inches),
+   * which is why the values are so specific.
+   * 
+   * @param {[slide]} slide
+   */
+const formatPage = (slide) => {
+  slide.addText("CONFIDENTIAL", {
+    y: 0.06,
+    w: "100%",
+    h: 0.29,
+    align: "center",
+    bold: true,
+    fontSize: 12,
+  });
+
+  slide.addText(
+    [
+      { text: `INCIDENT NUMBER: /${incNumb}`, options: { breakLine: true } },
+      { text: `LOCATION OF FIRE: ${location}`, options: { breakLine: true } },
+      { text: `                                   SINGAPORE ${postalCode}`} // Extra white space for formatting
+    ],
+    { x: 0.6653543, y: 0.4291339, h: 0.6968504, w: 5.5472441, fontSize: 12 }
+  );
+
+  slide.addText(`ANNEX ${annex}`, {
+    x: 6.2165354331,
+    y: 0.4291339,
+    h: 0.3425197,
+    w: 1.14173,
+    fontSize: 15,
+    bold: true,
+    underline: true
+  });
+
+  slide.addText(
+    [
+      { text: `${annex}-${pageNumb}`, options: { breakLine: true } },
+      { text: "CONFIDENTIAL"}
+    ],
+    { y: 10.429134, w: "100%", h: 0.492126, bold: true, fontSize: 12, align: "center" }
+  )
+}
+
+/**
+ * Formats the slide if it is a slide which requires drawing (e.g. Annex A)
+ * @param {slide} slide 
+ * @param {bool} layout If the drawing is for the layout plan of the affected area
+ */
+const formatDrawingTemplate = (slide, layout) => {
+  formatPage(slide);
+  slide.addShape(pptx.shapes.RECTANGLE, { 
+    x: 0.4173228,
+    y: 1.511811,
+    w: 6.8346457,
+    h: 8.7559055,
+    line: { width: 2.25 }
+  });
+
+  slide.addShape(pptx.shapes.LINE, {
+    x: 0.4173228,
+    y: 9.2637795,
+    w: 6.8188976,
+    h: 0,
+    line: { width: 2.25 }
+  });
+
+  // For the location of "sketch" and "legend" text
+  let sketchX, legendX, y;
+
+  if (layout) {
+    slide.addShape(pptx.shapes.LINE, {
+      x: 3.334646,
+      y: 9.2637795,
+      w: 0,
+      h: 1,
+      line: { width: 2.25 }
+    });
+
+    slide.addShape(pptx.shapes.RECTANGLE, {
+      x: 3.58268,
+      y: 9.5393701,
+      w: 0.3346457,
+      h: 0.2519685,
+      line: { width: 1 }
+    });
+
+    slide.addText("THE AFFECTED AREA", {
+      x: 4,
+      y: 9.5393701,
+      w: 1.41732,
+      h: 0.2519685,
+      fontSize: 8,
+    });
+
+    slide.addShape(pptx.shapes.OVAL, {
+      x: 3.58268,
+      y: 9.8582677,
+      w: 0.3346457,
+      h: 0.3346457,
+      line: {width : 1.5, color: "#FC0128" }
+    });
+
+    slide.addText("AREA OF FIRE ORIGIN", {
+      x: 4,
+      y: 9.8582677,
+      w: 1.41732,
+      h: 0.2519685,
+      fontSize: 8,
+    });
+
+    y = 9.2637795;
+    sketchX = 0.4173228;
+    legendX = 3.354331;
+
+  } else {
+    slide.addShape(pptx.shapes.LINE, {
+      x: 4.1653543,
+      y: 9.2637795,
+      w: 0,
+      h: 1,
+      line: { width: 2.25 }
+    });
+
+    y = 9.3385827;
+    sketchX = 0.5826772;
+    legendX = 4.3188976;
+  }
+
+  slide.addText("NOT DRAWN TO SCALE", {
+    x: 0.6338583,
+    y: 8.9685039,
+    w: 1.480315,
+    h: 0.23622,
+    fontSize: 8,
+    color: "919191",
+    underline: true,
+  });
+
+  slide.addText("SKETCH:", {
+    x: sketchX,
+    y: y,
+    h: 0.2598425,
+    w: 0.826772,
+    fontSize: 10,
+    underline: true
+  });
+  
+  slide.addText("LEGEND:", {
+    x: legendX,
+    y: y,
+    h: 0.2598425,
+    w: 0.826772,
+    fontSize: 10,
+    underline: true
+  });
+
+  annex = nextChar(annex);
+}
+
+
+/**
+ * Generates the photo log of the app and adds it to the ppt slide
+ * 
+ * @param {Array<Photo>} photos The array of photo objects
+ */
+function generatePhotoLog(photos) {
+
+  let basic = { bold: true, align: "center" };
+  
+  let rows = [[
+    { text: "Photo", options: basic },
+    { text: "Photo UID No.", options: basic },
+    { text: "Descriptions", options: basic },
+  ]];
+
+  photos.forEach((photo) => {
+    if (bag) {
+      photo.photoUIDNumb = `${bag}/${photo.photoNumb}`
+    }
+
+    photo.isLandscape = (photo.image.width > photo.image.height) ? true : false;
+
+    rows.push([
+      { text: photo.displayedNumb, options: { align: "center" } },
+      { text: photo.photoUIDNumb, options: basic },
+      photo.description
+    ]);
+  });
+
+  console.log(rows);
+
+  let newSlide = pptx.addSlide();
+  formatPage(newSlide);
+  newSlide.addTable(rows, {
+    x: 0.4173228,
+    y: 2.200787,
+    colW: [1.240157, 1.92913, 3.58268],
+    rowH: 0.488189,
+    h: 8,
+
+    valign: "middle",
+    border: { type: "solid" },
+    autoPage: true,
+    autoPageRepeatHeader: true,
+    autoPageSlideStartY: 2.200787,
+  });
+
+  newSlide.addText("TABLE OF PHOTO-LOG", {
+    x: 0.4094488,
+    y: 1.673228,
+    h: 0.3897638,
+    w: 3.165354,
+    fontSize: 18
+  });
+
+  pageNumb++;
+  
+  newSlide.newAutoPagedSlides.forEach((slide) => {
+    
+    formatPage(slide);
+    pageNumb++;
+    
+    slide.addText("TABLE OF PHOTO-LOG", {
+      x: 0.4094488,
+      y: 1.673228,
+      h: 0.3897638,
+      w: 3.165354,
+      fontSize: 18
+    });
+  });
+  
+  annex = nextChar(annex);
+  pageNumb = 1;
+}
+
+/**
+ * Generates the annex whereby the photos are displayed along with their descriptions
+ * 
+ * @param {Array<Photo>} photos 
+ */
+function generatePhotoAnnex(photos) {
+  
+  // The default Y position of photos if they are 4:3
+  const defaultY = {
+    l1: 1.326772, // First photo of landscape mode
+    l2: 5.9094488,
+  }
+
+  const defaultX = {
+    l: 1.251969, // X location for landscape pictures
+
+  }
+
+  const width = {
+    landscape: 5.0748031
+  }
+
+  for (let i = 0; i < photos.length; i++) {
+
+    let slide = pptx.addSlide();
+    formatPage(slide);
+
+    let firstPhoto = photos[i];
+    let firstRatio = getAspectRatio(firstPhoto);
+
+    if (firstPhoto.isLandscape) {
+
+      slide.addImage({
+        x: defaultX.l,
+        y: defaultY.l1 + (3.5 - width.landscape * (1/firstRatio)), // Need to account for the difference in height for different aspect ratios
+        path: firstPhoto.image.src,
+        sizing: { type: "contain", w: width.landscape }
+      });
+
+      const topTextY = 4.8937008;
+      const topTextHeight = 0.2007874;
+
+      const leftTopTextFormat = {
+        x: 1.251969,
+        w: 2.5,
+        h: topTextHeight,
+      };
+
+      const rightTopTextFormat = {
+        x: '50%',
+        w: 2.574803,
+        h: topTextHeight,
+      };
+
+      const descriptionFormat = {
+        x: 1.2519685,
+        h: 0.708661,
+        w: 5.0748031,
+      }
+
+      slide.addText(`PHOTO ${firstPhoto.numb}`, {
+        ...leftTopTextFormat,
+        y: topTextY,
+      });
+
+      slide.addText(firstPhoto.photoUIDNumb, {
+        ...rightTopTextFormat,
+        y: topTextY
+        
+      });
+
+      slide.addText(firstPhoto.description, {
+        ...descriptionFormat,
+        y: 5.0984252,
+      });
+
+      if (i < photos.length - 1) {
+        // The second photo must be of the same orientation and not have a copy
+        if (photos[i + 1].isLandscape && !photos[i + 1].hasCopy) {
+          i++;
+          let secondPhoto = photos[i];
+          let secondRatio = getAspectRatio(secondPhoto);
+
+          slide.addImage({
+            x: defaultX.l,
+            y: defaultY.l2 + (3.5 - width.landscape * (1/secondRatio)), // Need to account for the difference in height for different aspect ratios
+            path: secondPhoto.image.src,
+            sizing: { type: "contain", w: width.landscape }
+          });
+
+          const bottomTextY = 9.488189;
+
+          slide.addText(`${secondPhoto.copyOf ? "COPY OF" : ""} PHOTO ${secondPhoto.numb}`, {
+            x: defaultX.l,
+            y: bottomTextY,
+            h: topTextHeight,
+
+
+          });
+
+
+        }
+      }
+
+    } else {
+      // If two portrait photo on the slide
+      if (!photos[i + 1].isLandscape && !photos[i + 1].hasCopy) {
+        let secondPhoto = photos[i + 1];
+        i++;
+      } else {
+
+      }
+    }
+
+    pageNumb++;
+    
+  }
+}
 
 /**
  * Generates the ppt report and downloads it on user's computer
@@ -19,260 +429,9 @@ nextChar('a');
 async function generateReport(photos) {
 
   console.log(photos);
+  updateBasicConstants();
 
-  // Getting constants from local storage
-  const bag = localStorage.getItem("bagNumb");
-  const c1Acc = (localStorage.getItem("c1acc") === null) ? false : JSON.parse(localStorage.getItem("c1acc"));
-  const incNumb = localStorage.getItem("incidentNumb");
-  const location = localStorage.getItem("location").toUpperCase();
-  const postalCode = (localStorage.getItem("postalCode"));
-  
-  console.log(c1Acc);
-
-  // Formatting of slide
-  let pptx = new pptxgen();
-  
-  pptx.subject = "Fire Report";
-  pptx.theme = { headFontFace: "Arial", bodyFontFace: "Arial" };
-  pptx.defineLayout({ name:'A4', width:7.5, height:11.0244094 });
-  pptx.layout = "A4";
-  
-  let annex = "A";
-  let pageNumb = 1;
-  
-  /**
-   * Adds the basic formatting to each slide. Specific values taken with reference from original ppt (in inches),
-   * which is why the values are so specific.
-   * 
-   * @param {[slide]} slide
-   */
-  const formatPage = (slide) => {
-    slide.addText("CONFIDENTIAL", {
-      y: 0.06,
-      w: "100%",
-      h: 0.29,
-      align: "center",
-      bold: true,
-      fontSize: 12,
-    });
-
-    slide.addText(
-      [
-        { text: `INCIDENT NUMBER: /${incNumb}`, options: { breakLine: true } },
-        { text: `LOCATION OF FIRE: ${location}`, options: { breakLine: true } },
-        { text: `                                   SINGAPORE ${postalCode}`} // Extra white space for formatting
-      ],
-      { x: 0.6653543, y: 0.4291339, h: 0.6968504, w: 5.5472441, fontSize: 12 }
-    );
-
-    slide.addText(`ANNEX ${annex}`, {
-      x: 6.2165354331,
-      y: 0.4291339,
-      h: 0.3425197,
-      w: 1.14173,
-      fontSize: 15,
-      bold: true,
-      underline: true
-    });
-
-    slide.addText(
-      [
-        { text: `${annex}-${pageNumb}`, options: { breakLine: true } },
-        { text: "CONFIDENTIAL"}
-      ],
-      { y: 10.429134, w: "100%", h: 0.492126, bold: true, fontSize: 12, align: "center" }
-    )
-  }
-
-  /**
-   * Formats the slide if it is a slide which requires drawing (e.g. Annex A)
-   * @param {slide} slide 
-   * @param {bool} layout If the drawing is for the layout plan of the affected area
-   */
-  const formatDrawingTemplate = (slide, layout) => {
-    formatPage(slide);
-    slide.addShape(pptx.shapes.RECTANGLE, { 
-      x: 0.4173228,
-      y: 1.511811,
-      w: 6.8346457,
-      h: 8.7559055,
-      line: { width: 2.25 }
-    });
-
-    slide.addShape(pptx.shapes.LINE, {
-      x: 0.4173228,
-      y: 9.2637795,
-      w: 6.8188976,
-      h: 0,
-      line: { width: 2.25 }
-    });
-
-    // For the location of "sketch" and "legend" text
-    let sketchX, legendX, y;
-
-    if (layout) {
-      slide.addShape(pptx.shapes.LINE, {
-        x: 3.334646,
-        y: 9.2637795,
-        w: 0,
-        h: 1,
-        line: { width: 2.25 }
-      });
-
-      slide.addShape(pptx.shapes.RECTANGLE, {
-        x: 3.58268,
-        y: 9.5393701,
-        w: 0.3346457,
-        h: 0.2519685,
-        line: { width: 1 }
-      });
-
-      slide.addText("THE AFFECTED AREA", {
-        x: 4,
-        y: 9.5393701,
-        w: 1.41732,
-        h: 0.2519685,
-        fontSize: 8,
-      });
-
-      slide.addShape(pptx.shapes.OVAL, {
-        x: 3.58268,
-        y: 9.8582677,
-        w: 0.3346457,
-        h: 0.3346457,
-        line: {width : 1.5, color: "#FC0128" }
-      });
-
-      slide.addText("AREA OF FIRE ORIGIN", {
-        x: 4,
-        y: 9.8582677,
-        w: 1.41732,
-        h: 0.2519685,
-        fontSize: 8,
-      });
-
-      y = 9.2637795;
-      sketchX = 0.4173228;
-      legendX = 3.354331;
-
-    } else {
-      slide.addShape(pptx.shapes.LINE, {
-        x: 4.1653543,
-        y: 9.2637795,
-        w: 0,
-        h: 1,
-        line: { width: 2.25 }
-      });
-
-      y = 9.3385827;
-      sketchX = 0.5826772;
-      legendX = 4.3188976;
-    }
-
-    slide.addText("NOT DRAWN TO SCALE", {
-      x: 0.6338583,
-      y: 8.9685039,
-      w: 1.480315,
-      h: 0.23622,
-      fontSize: 8,
-      color: "919191",
-      underline: true,
-    });
-
-    slide.addText("SKETCH:", {
-      x: sketchX,
-      y: y,
-      h: 0.2598425,
-      w: 0.826772,
-      fontSize: 10,
-      underline: true
-    });
-    
-    slide.addText("LEGEND:", {
-      x: legendX,
-      y: y,
-      h: 0.2598425,
-      w: 0.826772,
-      fontSize: 10,
-      underline: true
-    });
-
-    annex = nextChar(annex);
-  }
-
-  /**
-   * Generates the photo-log of the given project
-   */
-  const generatePhotoLog = () => {
-
-    let basic = { bold: true, align: "center" };
-    
-    let rows = [[
-      { text: "Photo", options: basic },
-      { text: "Photo UID No.", options: basic },
-      { text: "Descriptions", options: basic },
-    ]];
-
-    photos.forEach((photo) => {
-      if (bag) {
-        photo.photoUIDNumb = `${bag}/${photo.photoNumb}`
-      }
-
-      photo.isLandscape = (photo.image.width > photo.image.height) ? true : false;
-
-      rows.push([
-        { text: photo.displayedNumb, options: { align: "center" } },
-        { text: photo.photoUIDNumb, options: basic },
-        photo.description
-      ]);
-    });
-
-    console.log(rows);
-
-    let newSlide = pptx.addSlide();
-    formatPage(newSlide);
-    newSlide.addTable(rows, {
-      x: 0.4173228,
-      y: 2.200787,
-      colW: [1.240157, 1.92913, 3.58268],
-      rowH: 0.488189,
-      h: 8,
-
-      valign: "middle",
-      border: { type: "solid" },
-      autoPage: true,
-      autoPageRepeatHeader: true,
-      autoPageSlideStartY: 2.200787,
-    });
-
-    newSlide.addText("TABLE OF PHOTO-LOG", {
-      x: 0.4094488,
-      y: 1.673228,
-      h: 0.3897638,
-      w: 3.165354,
-      fontSize: 18
-    });
-
-    pageNumb++;
-    
-    newSlide.newAutoPagedSlides.forEach((slide) => {
-      
-      formatPage(slide);
-      pageNumb++;
-      
-      slide.addText("TABLE OF PHOTO-LOG", {
-        x: 0.4094488,
-        y: 1.673228,
-        h: 0.3897638,
-        w: 3.165354,
-        fontSize: 18
-      });
-    });
-    
-    annex = nextChar(annex);
-    pageNumb = 1;
-  }
-
+  formatPptx();
 
   const leftEdge = 0.4173228;
 
@@ -345,7 +504,7 @@ async function generateReport(photos) {
   );
   
   if (!c1Acc) {
-    generatePhotoLog();
+    generatePhotoLog(photos);
 
     console.log(photos.at(-1));
 
@@ -365,44 +524,12 @@ async function generateReport(photos) {
         align: "center"
       });
   }
-
-  for (let i = 0; i < photos.length; i++) {
-
-    let slide = pptx.addSlide();
-    formatPage(slide);
-
-    let firstPhoto = photos[i];
-    if (firstPhoto.isLandscape) {
-      //TODO: Format texts and pictures
-      // TODO: Note that you can resize the picture using pptxgenjs
-      
-      slide.addImage({
-        x: 1, // TODO: Calculate based on difference in image height
-        y: 1,
-        sizing: { }
-      })
-
-      // The second photo must be of the same orientation and not have a copy
-      if (photos[i + 1].isLandscape && !photos[i + 1].hasCopy) {
-        let secondPhoto = photos[i + 1];
-        i++;
-      }
-
-    } else {
-      // If two portrait photo on the slide
-      if (!photos[i + 1].isLandscape && !photos[i + 1].hasCopy) {
-        let secondPhoto = photos[i + 1];
-        i++;
-      } else {
-
-      }
-    }
-
-    pageNumb++;
-    
-  }
+  
+  generatePhotoAnnex(photos);
 
   // TODO: Add signature slide
+  pageNumb = 1;
+
   // TODO: Add injury
 
   pptx.writeFile({ fileName: `${incNumb.replace("/", "-")}-location` });
