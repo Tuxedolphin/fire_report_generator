e490c646a7b149c5e45b0988209aa6efeda5bfe3
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-10-15 22:16:14 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    chunkSplitPlugin()
+  ],
+  
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+  },
+
+  base: "/fire_report_generator/"
 })
